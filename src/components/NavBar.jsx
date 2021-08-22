19b3a78ed5d1814c4f4a3c60eefde1c0821a5f99
SiriusLL <sirius.love.light@gmail.com>
2021-08-21 21:28:45 -0600

    navbar skeleton

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 function NavBar() {
-  return <div>navBar</div>;
+  return (
+    <>
+      <div>logo</div>
+      <div>
+        <ul>
+          <li>logo</li>
+          <li>one</li>
+          <li>two</li>
+          <li>three</li>
+          <li>four</li>
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default NavBar;
