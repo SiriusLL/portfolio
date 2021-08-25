7b5325a16b8a10e2956576f496db4532784f5772
SiriusLL <sirius.love.light@gmail.com>
2021-08-24 21:55:48 -0600

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import "./Hero.css";
 
 function Hero() {
-  return <div className="hero">Hero</div>;
+  return (
+    <div className="hero">
+      <video src="/videos/video-1.mp4" autoPlay loop muted />
+      <h1>ADVENTURE AWAITS</h1>
+      <p>What are you waiting for?</p>
+    </div>
+  );
 }
 
 export default Hero;
