167fe6aaf217370222e84ebdacb59953535d4b10
SiriusLL <sirius.love.light@gmail.com>
2021-08-25 22:59:28 -0600

    save

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
+import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
       <Hero />
       <NavBar />
+      <Main />
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,11 +2,27 @@ import React from "react";
 import "./NavBar.css";
 
 function NavBar() {
-  const nav = document.querySelector("#main");
+  // const nav = document.querySelector("#navb");
+  // ReactDOM.render(nav);
+  // console.log("nav", nav);
+  // const topOfNav = nav.offsetTop;
+
+  // function fixNav() {
+  //   // console.log(topOfNav, window.scrollY);
+  //   if (window.scrollY >= topOfNav) {
+  //     document.body.style.paddingTop = nav.offsetHeight + "px";
+  //     document.body.classList.add("fixed-nav");
+  //   } else {
+  //     document.body.style.paddingTop = 0;
+  //     document.body.classList.remove("fixed-nav");
+  //   }
+  // }
+  useEffect(() => {});
+  window.addEventListener("scroll", fixNav);
 
   function fixNav() {}
   return (
-    <div className="navbar">
+    <div id="navb" className="navbar">
       {/* <div>logo</div> */}
       <nav className="nav-menu">
         <ul>
