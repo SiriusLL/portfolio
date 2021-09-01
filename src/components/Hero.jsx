e0afce0502a010d9e715c06157042ee390d53dab
SiriusLL <sirius.love.light@gmail.com>
2021-09-01 16:38:25 -0600

    position hero-content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,8 +5,10 @@ function Hero() {
   return (
     <div className="hero">
       <video src="/videos/video-1.mp4" autoPlay loop muted />
-      <h1>ADVENTURE AWAITS</h1>
-      <p>What are you waiting for?</p>
+      <div className="header-content">
+        <h1>HELLO!!</h1>
+        <p>My name is James</p>
+      </div>
     </div>
   );
 }
