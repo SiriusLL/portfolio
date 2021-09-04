782a32b47ee5b13c7ecb51bf37f464f74ac9c9e8
SiriusLL <sirius.love.light@gmail.com>
2021-09-03 23:20:20 -0600

    start projects section, add admin section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,19 @@
 import "./Projects.css";
 
 const Projects = () => {
+  const projectList = [
+    "project1",
+    "project2",
+    "project3",
+    "project4",
+    "project5",
+  ];
+  const projectCarasole = projectList.map((projectItem, index) => {
+    return <div className={`project${a + index}`}>{projectItem}</div>;
+  });
   return (
     <div className="projects-section" id="projects">
-      Projects
+      {projectCarasole}
     </div>
   );
 };
